e35e29cf58aa5148452ad74a3856ec41b98da02b
innocurve <hjpw96@naver.com>
2025-02-19 14:54:26 +0900

    인덱스 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
 import Home from './_app'; // home.tsx를 import
+import Head from 'next/head'
 
 const IndexPage = () => {
-  return <Home />; // Home 컴포넌트를 렌더링
+  return (
+    <>
+      <Head>
+        <title>AIGF 2025: 모두를 위한 AI와 디지털 미래</title>
+        <meta name="description" content="디지털 미래 세미나에 귀하를 초대합니다." />
+        
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="AIGF 2025: 모두를 위한 AI와 디지털 미래" />
+        <meta property="og:description" content="디지털 미래 세미나에 귀하를 초대합니다." />
+        <meta property="og:image" content="https://daecheongse.co.kr/dcsLogo.jpg" /> {/* 대표 이미지가 있다면 경로 지정 */}
+        
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="AIGF 2025: 모두를 위한 AI와 디지털 미래" />
+        <meta name="twitter:description" content="디지털 미래 세미나에 귀하를 초대합니다." />
+        <meta name="twitter:image" content="https://daecheongse.co.kr/dcsLogo.jpg" /> {/* 대표 이미지가 있다면 경로 지정 */}
+      </Head>
+      
+      <Home />
+    </>
+  );
 };
 
 export default IndexPage;
